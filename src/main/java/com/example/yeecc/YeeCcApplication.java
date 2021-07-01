51102fab1f72385ab0143bb7e2748c9c91f3fa21
Administrator <961916428@qq.com>
2021-07-01 22:50:15 +0800

    first commit

---
+++ src/main/java/com/example/yeecc/YeeCcApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeeCcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeeCcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yeecc/YeeCcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeecc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeeCcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
